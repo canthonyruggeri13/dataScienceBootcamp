1f7e6a6d37825064caab718209d258ee5c08f74e
canthonyruggeri13 <44042998+canthonyruggeri13@users.noreply.github.com>
2018-12-01 17:44:30 -0600

    Add files via upload
    hw2 submission

---
+++ SubFOLDhw2/2014macro.vb
@@ -0,0 +1,37 @@
+Sub VbaHw()
+
+
+Dim ticker As String
+Dim vol As Double
+vol = 0
+Dim SummaryTableRow As Integer
+SummaryTableRow = 2
+
+Cells(1, 9).Value = "Ticker Name"
+Cells(1, 10).Value = "Total Volume"
+
+
+
+For i = 2 To 705714
+   If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
+       ticker = Cells(i, 1).Value
+
+   Range("I" & SummaryTableRow).Value = ticker
+       vol = vol + Cells(i, 7).Value
+
+    Range("J" & SummaryTableRow).Value = vol
+
+
+
+       SummaryTableRow = SummaryTableRow + 1
+      vol = 0
+
+
+   Else
+   vol = vol + Cells(i, 7).Value
+
+   End If
+   Next i
+   
+   End Sub
+   
